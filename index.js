0e6fedea833e8215258ab5c9f4cfe9ce1a157cfb
DonaldKLee <Donald3010@outlook.com>
2023-09-16 02:40:22 -0400

    post resume to node

--- index.js
+++ index.js
@@ -8,10 +8,12 @@ app.get('/', async(req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// app.post('/uploadResume', (req, res) => {
-//   const { pdfFile } = req.body;
-//   res
-// })
+app.post('/uploadResume', (req, res) => {
+  // const { pdfFile } = req.body.resume;
+  const pdfFile = req;
+  console.log(pdfFile);
+  res.sendFile(path.join(__dirname, 'public', 'interview.html'));
+})
 
 app.listen(8080, () => {
     console.log("Server successfully running on port 8080");
--- public/js/pdfToText.js
+++
@@ -1,3 +0,0 @@
-function pdfFileSubmitted() {
-    alert("hd");
-}
