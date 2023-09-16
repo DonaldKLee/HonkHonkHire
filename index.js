9d7f9408bad7d0bb8cb4cfb838940807c5debc04
DonaldKLee <Donald3010@outlook.com>
2023-09-16 02:11:41 -0400

    made form

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ app.get('/', async(req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// app.post('/uploadResume', (req, res) => {
+//   const { pdfFile } = req.body;
+//   res
+// })
+
 app.listen(8080, () => {
     console.log("Server successfully running on port 8080");
   });
---
+++ public/js/pdfToText.js
@@ -0,0 +1,3 @@
+function pdfFileSubmitted() {
+    alert("hd");
+}
