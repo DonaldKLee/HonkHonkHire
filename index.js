d56305610a0490c404bcf24507b962d47a34b669
michelleykim <yeojin011016@gmail.com>
2023-09-16 18:26:55 -0700

    implement main dashboard page

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const session = require('express-session');
-const cors = require('cors');
-const authRoute = require('./routes/auth');
-const audioRoute = require('./routes/audio');
-const quizRoute = require('./routes/quiz');
-require('dotenv').config();
-  
+const mongoose = require("mongoose");
+const session = require("express-session");
+const cors = require("cors");
+const authRoute = require("./routes/auth");
+const audioRoute = require("./routes/audio");
+const quizRoute = require("./routes/quiz");
+require("dotenv").config();
+
 //MIDDLEWARES
-app.use(express.static(__dirname + '/public'));
-app.set('views', (__dirname + '/views'))
-app.set('view engine', 'ejs');
+app.use(express.static(__dirname + "/public"));
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
 app.use(cors());
 app.use(
 	session({
@@ -27,17 +27,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // MONGODB CONNECTION
-mongoose.set('strictQuery', true);
-mongoose.connect(process.env.MONGO_URI,{ 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true
-}).then(() => console.log('MongoDB Connected'))
-.catch(err => console.log(err))
+mongoose.set("strictQuery", true);
+mongoose
+	.connect(process.env.MONGO_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log("MongoDB Connected"))
+	.catch((err) => console.log(err));
 
 //ROUTES
-app.use('/auth', authRoute);
-app.use('/audio', audioRoute);
-app.use('/quiz', quizRoute);
+app.use("/auth", authRoute);
+app.use("/audio", audioRoute);
+app.use("/quiz", quizRoute);
 
 // ROOT
 app.get("/", (req, res) => {
@@ -50,6 +52,10 @@ app.get("/onboarding", (req, res) => {
 
 app.get("/interview", (req, res) => {
 	res.render("interviewPage.ejs");
+});
+
+app.get("/dashboard", (req, res) => {
+	res.render("dashboardPage.ejs");
 });
 
 app.get("/audio", (req, res) => {
